045e019fa9cd33fdcecd74284fb9e7b4756aae04
Christian Axel Serrano Sandoval ( Chiles ) <61697287+Chiles012@users.noreply.github.com>
2023-02-23 07:31:24 -0600

    trabajo incompleto

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const axios = require("axios");
+var fs = require('fs-extra');
+var request = require('request');
 
-axios.get("https://pokeapi.co/api/v2/pokemon/ditto").then(data => {
-    console.log(data);
-})
+// Traemos el link desde un archivo txt
+const link = fs.readFileSync('./link.txt', 'utf-8');
+
+// Traer el html de la pagina
+request
+  .get(link)
+  .on('response', function(response) {
+    console.log(response.statusCode) // 200
+    console.log(response.headers['content-type']) // 'image/png'
+  })
